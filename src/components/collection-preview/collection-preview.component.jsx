6ab7108121e2e8acb78b225c5ba6d771e10eeaa6
Amol shelke <shelkeamol897@gmail.com>
2022-08-10 19:20:29 +0530

    Converted Collection Overview and Collection Preview component over to styled component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-import './collection-preview.styles.scss';
+import {
+  CollectionPreviewContainer,
+  PreviewTitle,
+  PreviewContainer,
+} from './colllection-preview.styles';
 
 const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+  <CollectionPreviewContainer>
+    <PreviewTitle>{title.toUpperCase()}</PreviewTitle>
+    <PreviewContainer>
       {items
         .filter((item, idx) => idx < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </PreviewContainer>
+  </CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
