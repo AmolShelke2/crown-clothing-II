b100ebd8e50ed6e72a91c55816eb533cd3a0354d
Amol shelke <shelkeamol897@gmail.com>
2022-08-11 11:29:05 +0530

    Converted SignIn and SingUp Page over styled component

--- src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
+++ src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-import './sign-up-and-sign-in.styles.scss';
-
+import { SingInandSignUpPage } from './sign-up-and-sign-in.styles';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up">
+  <SingInandSignUpPage>
     <SignIn />
     <SignUp />
-  </div>
+  </SingInandSignUpPage>
 );
 
 export default SignInAndSignUpPage;
