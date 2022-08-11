c56e8045c64337387308d9a9c5a1b35a5d92d380
Amol shelke <shelkeamol897@gmail.com>
2022-08-11 12:58:44 +0530

    Converted Collection Page over to Styled Component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,21 +3,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop-selector';
 
-import './collection.styles.scss';
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import {
+  CollectionPageContainer,
+  TitleContainer,
+  ItemsContainer,
+} from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
 
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <TitleContainer>{title}</TitleContainer>
+      <ItemsContainer>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </ItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
@@ -26,5 +30,3 @@ const mapStateToProps = (state, ownProps) => ({
 });
 
 export default connect(mapStateToProps)(CollectionPage);
-
-// Thinking of DATA Flow
