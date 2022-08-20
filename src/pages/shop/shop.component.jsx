2a9d631bb1d85b7a86c61c32129a5fe517ee9ab0
Amol shelke <shelkeamol897@gmail.com>
2022-08-20 14:46:24 +0530

    Learnt Promise Pattern and Also Fetched The Data From Firebase API Endpoints

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,13 +28,19 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collection');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ loading: false });
-      }
-    );
+    // FETCH PATTERN || PROMISE PATTERN
+
+    // fetch(
+    //   'https://firestore.googleapis.com/v1/projects/crown-db-81294/databases/(default)/documents/collection'
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
+
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   render() {
